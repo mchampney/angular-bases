2891e1a3a3f1efb08be2a0000c9a11c5220f939c
Michelle Champney <champney.miichelle9619@gmail.com>
2024-08-12 10:30:27 -0600

    seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
-import { HeroModule } from './heros/hero.component';
+import { HeroModule } from './heros/hero.module';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent, CounterComponent],
-  imports: [BrowserModule, AppRoutingModule, HeroModule],
+  imports: [BrowserModule, AppRoutingModule, HeroModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
